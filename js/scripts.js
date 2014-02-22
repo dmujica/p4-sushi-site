f771af5d6d948d0fc9c6d9e4d0b9e302dbe47fc6
dmujica <daniellemujica@yahoo.com>
2014-02-22 00:16:40 -0800

    added map

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,15 @@ $(document).ready(function(){
 
 	$(".main").onepage_scroll();
 
+		new Maplace({
+		show_markers: true,
+		locations: [{
+			lat: 47.659381, 
+			lon: -117.4236639,
+			zoom: 18
+		}]
+	}).Load(); 
+
 	console.log('Page Loaded. Lets Do this!');
 
 
